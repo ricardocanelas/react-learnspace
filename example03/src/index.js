f3ea95f08b546a473e0c12947ddfb4ebd6601b1f
Ricardo Canelas <hello@ricardocanelas.com>
2018-12-01 14:17:00 -0200

    removed all examples

--- example03/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './utils/registerServiceWorker';
-
-import App from './App';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
