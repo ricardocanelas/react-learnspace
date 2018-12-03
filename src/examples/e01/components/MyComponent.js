4f87cced7bcafc8b17a969279b02ec25b08cd80f
Ricardo Canelas <hello@ricardocanelas.com>
2018-12-03 10:53:52 -0200

    added e01

---
+++ src/examples/e01/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { withCounter } from './Counter';
+
+const MyComponent = (props) => {
+    return (
+        <div>
+            Counter: {props.counter}
+            <button onClick={props.increment}>+1</button>
+        </div>
+    )
+}
+
+export default withCounter(MyComponent)
--- src/examples/e01/index.js
+++ src/examples/e01/index.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Counter from './components/Counter'
+import MyComponent from './components/MyComponent';
 
-export default () => {
-    return(
-        <div>Example 01</div>
-    )
+import './index.css';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+
+                {/* Render Props */}
+                <Counter render={({ counter, increment }) => (
+                    <div>
+                        Counter: {counter}
+                        <button onClick={increment}>Add +1</button>
+                    </div>
+                )} />
+
+                {/* HOC */}
+                <MyComponent />
+            </div>
+        );
+    }
 }
+
+export default App;
